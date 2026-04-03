d85b2d449bb64ce1d15c926e0abfd9799e348891
giobbu <giovanniburoni@gmail.com>
2026-04-03 14:07:22 +0200

    update data generation script and notebook

--- notebooks/gps-analytics/helpers/generate_poisson_ar1.py
+++ notebooks/gps-analytics/helpers/generate_poisson_ar1.py
@@ -1,6 +1,7 @@
 # Reference: https://observablehq.com/@mattiasvillani/poisson-time-series-model
 import json
 import numpy as np
+import datetime
 
 def simulate_poisson_ar1(n: int = 300, mu: float = 1.0, phi: float = 0.90, sigma_eta: float = 0.4, seed: int = 42) -> dict:
 
@@ -32,7 +33,13 @@ def simulate_poisson_ar1(n: int = 300, mu: float = 1.0, phi: float = 0.90, sigma
         lam[t] = np.exp(z[t])
         y[t]   = rng.poisson(lam[t])
 
-    return {"t": np.arange(n).tolist(), 
+    list_timestamps = [f"2024-01-01T00:00:00Z"]
+    for i in range(1, n):
+        next_timestamp = (datetime.datetime.fromisoformat(list_timestamps[-1].replace("Z", "")) + datetime.timedelta(hours=1)).isoformat() + "Z"
+        list_timestamps.append(next_timestamp)
+
+    return {"timestamp": list_timestamps,
+            "t": np.arange(n).tolist(), 
             "z": z.tolist(),
             "lam": lam.tolist(), 
             "y": y.tolist(),
@@ -47,6 +54,8 @@ def simulate_poisson_ar1(n: int = 300, mu: float = 1.0, phi: float = 0.90, sigma
 
 if __name__ == "__main__":
 
+    import pandas as pd
+
     # set parameters for simulation
     n=300
     mu=1.0
@@ -59,7 +68,14 @@ if __name__ == "__main__":
         sim = simulate_poisson_ar1(n = 300, mu = 1.0, phi = .9, sigma_eta = 0.4, seed = seed)
         list_ts.append(sim)
 
-    with open(f"poisson_ar1_sim_{len(seeds)}_seeds.json", "w") as f:
-        json.dump(list_ts, f, indent=2)
+    # convert to Dataframe with timestamp as index, y1, y2 as columns and save it as Parquet file
+    df = pd.DataFrame({
+        "timestamp": list_ts[0]["timestamp"],
+        "y1": list_ts[0]["y"],
+        "y2": list_ts[1]["y"],
+    })
+    print("Simulated DataFrame:")
+    print(df.head())
 
-    #plot(sim, save_path=None)
+    df.to_parquet("simulated_poisson_ar1.parquet", index=False)
+    print("Simulated data saved to 'simulated_poisson_ar1.parquet'")
